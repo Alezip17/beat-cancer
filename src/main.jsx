c441d07b9adb9a0ab2be71cf462bcb32e72dc2cd
Alezip17 <alexasierra1707@gmail.com>
2025-03-26 16:23:34 -0500

    new

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <PrivyProvider
-    appId="clz5th2t100r3sbdu44h7zzn6"
+    appId="cm8nmaazh00azfqy49oah8wew"
     config={{
       appearance: {
         theme: "dark",
